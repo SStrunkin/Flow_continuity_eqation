e442978078391594c62913415af6a84045b21215
SStrunkin <electron-step@mail.ru>
2022-02-08 20:52:44 +0300

    Added all files in project

--- Flow_continiuty_eqation/MainWindow.xaml.cs
+++ Flow_continiuty_eqation/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace Flow_continiuty_eqation
     /// </summary>
     public partial class MainWindow : Window
     {
+        PipeSection pipeSectionLeft = new PipeSection();
+        PipeSection pipeSectionRight = new PipeSection();
+
+       
+
         public MainWindow()
         {
             InitializeComponent();
+
+            sqareTextBoxLeft.Text = "0";
+            sqareTextBoxRight.Text = "0";
+
+            
+            BitmapImage bi3 = new BitmapImage();
+            bi3.BeginInit();
+            bi3.UriSource = new Uri("/assets/little.bmp", UriKind.Relative);
+            bi3.EndInit();
+            mainImageLeft.Stretch = Stretch.Fill;
+            mainImageLeft.Source = bi3;
+
+            BitmapImage bi4 = new BitmapImage();
+            bi4.BeginInit();
+            bi4.UriSource = new Uri("/assets/big.bmp", UriKind.Relative);
+            bi4.EndInit();
+            mainImageRight.Stretch = Stretch.Fill;
+            mainImageRight.Source = bi4;
         }
+
+
+        private void setMainPictures()
+        {
+
+        }
+        
     }
 }

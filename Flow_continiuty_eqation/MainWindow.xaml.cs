e1a14e645db2f222d3af68a0719d278d7390084e
SStrunkin <electron-step@mail.ru>
2022-02-09 13:17:17 +0300

    Start write set main pictures functon

--- Flow_continiuty_eqation/MainWindow.xaml.cs
+++ Flow_continiuty_eqation/MainWindow.xaml.cs
@@ -23,36 +23,73 @@ namespace Flow_continiuty_eqation
         PipeSection pipeSectionLeft = new PipeSection();
         PipeSection pipeSectionRight = new PipeSection();
 
-       
+        BitmapImage littlePipe = new BitmapImage();
+        BitmapImage bigPipe = new BitmapImage();
+        
+      
+        
 
         public MainWindow()
         {
             InitializeComponent();
 
+
+            littlePipe.BeginInit();
+            littlePipe.UriSource = new Uri("/assets/little.bmp", UriKind.Relative);
+            littlePipe.EndInit();
+            mainImageLeft.Stretch = Stretch.Fill;
+
+            bigPipe.BeginInit();
+            bigPipe.UriSource = new Uri("/assets/big.bmp", UriKind.Relative);
+            bigPipe.EndInit();
+            mainImageRight.Stretch = Stretch.Fill;
+
+
+            sqareTextBoxLeft.TextChanged += setMainPictures;            
+            sqareTextBoxRight.TextChanged += setMainPictures;
             sqareTextBoxLeft.Text = "0";
             sqareTextBoxRight.Text = "0";
 
-            
-            BitmapImage bi3 = new BitmapImage();
-            bi3.BeginInit();
-            bi3.UriSource = new Uri("/assets/little.bmp", UriKind.Relative);
-            bi3.EndInit();
-            mainImageLeft.Stretch = Stretch.Fill;
-            mainImageLeft.Source = bi3;
-
-            BitmapImage bi4 = new BitmapImage();
-            bi4.BeginInit();
-            bi4.UriSource = new Uri("/assets/big.bmp", UriKind.Relative);
-            bi4.EndInit();
-            mainImageRight.Stretch = Stretch.Fill;
-            mainImageRight.Source = bi4;
         }
 
 
-        private void setMainPictures()
-        {
+        private void setMainPictures(object sender, TextChangedEventArgs e)
+        {                        
+            if ((sqareTextBoxLeft.Text != "") && (sqareTextBoxRight.Text != ""))
+            {
+                String leftSqareValueString = "", rightSqareValueString = "";
+                Double leftSqareValueDouble = 0, rightSqareValueDouble = 0;
 
+                leftSqareValueString = sqareTextBoxLeft.Text;
+                rightSqareValueString = sqareTextBoxRight.Text;
+
+                try
+                {
+                    leftSqareValueDouble = Double.Parse(leftSqareValueString);
+                    rightSqareValueDouble = Double.Parse(rightSqareValueString);
+                }
+                catch (Exception)
+                {}
+
+                if(leftSqareValueDouble == rightSqareValueDouble)
+                {
+                    mainImageLeft.Source = littlePipe;
+                    mainImageRight.Source = littlePipe;                   
+                }
+                else if(leftSqareValueDouble > rightSqareValueDouble)
+                {
+                    mainImageLeft.Source = bigPipe;
+                    mainImageRight.Source = littlePipe;                    
+                }
+                else if(leftSqareValueDouble < rightSqareValueDouble)
+                {
+                    mainImageLeft.Source = littlePipe;
+                    mainImageRight.Source = bigPipe;
+                }
+            }
         }
-        
+
+       
+
     }
 }
